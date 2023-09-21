b873be67b88dd688fc1ba72d896b7e2c658c9f2e
VeeA1 <vikakbary@gmail.com>
2023-09-21 13:24:59 +0700

    finalizing v.1.0 program

--- employee_db.py
+++ employee_db.py
@@ -24,7 +24,7 @@ def main_menu(i:int):
     elif i == 3:
         show_employee()
     elif i == 4:
-        pass
+        delete_menu()
     elif i == 5:
         print('Terima kasih telah mengakses daftar karyawan')
         print('-----------  Have a good day  --------------')
@@ -273,6 +273,113 @@ def edit_data(keys:str):
             print('Perintah tidak ditemukan!')
             print('Mohon pilih ulang (1-2):', end = ' ')
             user_input=input()
+
+def delete_menu():
+    print()
+    print('Apakah anda ingin menghapus data?')
+    print('1. Ya')
+    print('2. Tidak')
+    print('Input (1/2):', end = ' ')
+    user_input = int(input())
+    print()
+
+    if user_input > 1:
+        print('Perintah hapus dibatalkan')
+        print('Kembali ke menu awal')
+        main_menu_display()
+
+    print('=============  Delete Menu  =============')
+    print('1. Hapus data karyawan')
+    print('2. Hapus database')
+    print('3. Kembali ke menu awal')
+    print('Silahkan pilih perintah hapus')
+    print('Input (1-3):', end = ' ')
+    user_input = int(input())
+
+    if  user_input == 1:
+        delete_data()
+    elif  user_input == 2:
+        clear_data() 
+    elif  user_input == 3:    
+        print('Kembali ke menu awal')
+        main_menu_display()
+    else:
+        print('Perintah tidak ditemukan!')
+        print('Perintah delete dibatalkan')
+        print('Kembali ke menu awal')
+        main_menu_display()
+
+def delete_data():
+        print()
+        print('Masukkan nama lengkap karyawan:', end = ' ')
+        name_input = (input().lower()).title()
+
+        if name_input in employee_database.keys():
+            print('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(name_input,
+                                                          employee_database[name_input]['ID'],
+                                                          employee_database[name_input]['Tempat Lahir'],                                                   
+                                                          employee_database[name_input]['Tanggal Lahir'],
+                                                          employee_database[name_input]['Alamat'],
+                                                          employee_database[name_input]['Golongan Darah'],
+                                                          employee_database[name_input]['Agama'],
+                                                          employee_database[name_input]['Status Perkawinan']
+                                                          ))
+            print()
+            print('Apakah anda ingin menghapus data karyawan " {} "'.format(name_input))
+            print('1. Ya')
+            print('2. Tidak')
+            print('Input (1/2):', end = ' ')
+            user_input = int(input())
+            if user_input == 1:
+                del employee_database[name_input]
+                print('Data karyawan telah dihapus')
+                print('Kembali ke menu awal')
+                main_menu_display()
+            elif user_input == 2:
+                print('Perintah penghapusan dibatalkan')
+                print('Kembali ke menu awal')
+                main_menu_display()
+            else:
+                print()
+                print('Perintah tidak ditemukan!')
+                print('Mohon pilih ulang (1/2):', end = ' ')
+                user_input=input()
+        else:
+            print('Data karyawan tidak ditemukan!')
+            print('Perintah delete dibatalkan')
+            print('Kembali ke menu awal')
+            main_menu_display()
+
+def clear_data():
+    print()
+    print('Apakah anda ingin menghapus seluruh data karyawan?')
+    print('1. Ya')
+    print('2. Tidak')
+    print('Input (1/2):', end = ' ')
+    user_input = int(input())
+    print()
+
+    if user_input == 1:
+        print('Apakah anda yakin?')
+        print('1. Ya')
+        print('2. Tidak')
+        print('Input (1/2):', end = ' ')
+        user_input = int(input())
+        print()
+        
+        if user_input == 1:
+            employee_database.clear()
+            print('Database telah dikosongkan')
+            print('Kembali ke menu awal')
+            main_menu_display()
+        else:
+            print('Perintah hapus dibatalkan')
+            print('Kembali ke menu awal')
+            main_menu_display()
+    else:
+            print('Perintah hapus dibatalkan')
+            print('Kembali ke menu awal')
+            main_menu_display()
 
 employee_database={
     'Viky':{
