01c79b6072a46c5a84b88b9148307cfd48d96fc5
VeeA1 <vikakbary@gmail.com>
2023-09-25 00:27:46 +0700

    simplizing database display program

--- employee_record.py
+++ employee_record.py
@@ -1,54 +1,33 @@
-def database_menu_display():
-    print()
-    print('=============  Database Karyawan  =============')
-    print()
-    print('1. Tampilkan data seluruh karyawan')
-    print('2. Tampilkan data karyawan')
-    print('3. Kembali ke menu utama')
-    print()
-    print('Silahkan pilih menu yang ingin di akses (1-3):', end=' ')
-    user_input = int(input())
-    database_menu(user_input)
-
-def database_menu(i:int):
-    '''
-    Memilih menu yang diinginkan
-    '''
-    if i == 1:
-        show_database()
-    elif i == 2:
-        show_employee()
-    elif i == 3:
-        print('Kembali ke Menu Utama')
-        print()
-        # main_menu_display()
-    else:
-        print('Pilihan menu tidak tersedia')
-        print('Mohon pilih ulang menu yang yang tersedia')
-        print()
-        database_menu_display()
-
 def show_database():
     print()
+    print('======================================  Data Karyawan Divisi Data Science  =======================================')
+    print()
+    
+    global employee_database
+    employee_database = dict(sorted(employee_database.items()))
+    
     for employee_name, employee_data in employee_database.items():
         print('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(employee_name, 
                                                employee_data['ID'],
-                                               employee_data['Tempat Lahir'],
-                                               employee_data['Tanggal Lahir'],
+                                               employee_data['Tempat Lahir'],                                                   employee_data['Tanggal Lahir'],
                                                employee_data['Alamat'],
                                                employee_data['Golongan Darah'],
                                                employee_data['Agama'],
                                                employee_data['Status Perkawinan']
                                                ))
-    print()
+    # main_menu_display()
 
 def show_employee():
     print()
-    print('Masukkan nama lengkap karyawan:', end=' ')
+    print('--------------------------------------  Data Karyawan Divisi Data Science  --------------------------------------')
+    print()
+    print('Masukkan nama karyawan:', end=' ')
     name_input = input().lower()
     name_input = name_input.title()
     print()
 
+    print('--------------------------------------  Data Karyawan Divisi Data Science  --------------------------------------')
+    print()
     if name_input in employee_database.keys():
         print('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(name_input,
                                                       employee_database[name_input]['ID'],
@@ -62,47 +41,27 @@ def show_employee():
     else:
         print('Data karyawan tidak ditemukan!')
         print('Masukkan ulang nama karyawan')
-        print()
         show_employee()
+    print()
     
-    print('Apakah anda ingin mengedit data karyawan?')
+    print('Apakah anda ingin mengedit data karyawan ?')
     print('1. Ya')
     print('2. Tidak')
-    print('Input (1/2):', end = ' ')
-    user_input=int(input())
+    print('Input (1 / 2):', end = ' ')
+    user_input=input()
     
-    edit_database = True
-    while edit_database == True:
-        if user_input == 1:
+    while True:
+        if user_input == '1':
+            print()
             pass
-        elif user_input == 2:
-            print('Kembali ke Menu Database')
-            database_menu_display()
+            # edit_data(name_input)
+        elif user_input == '2':
+            print()
+            print('Kembali ke Main Menu')
+            pass
+            # main_menu_display()
         else:
+            print()
             print('Perintah tidak ditemukan!')
-            print('Mohon pilih ulang (1-2):', end = ' ')
-            user_input=int(input())
-
-
-employee_database={
-    'Viky':{
-        'ID' : 'DS150798',
-        'Tempat Lahir' : 'Surabaya',
-        'Tanggal Lahir' : '15 Juli 1998',
-        'Alamat' : 'Gayungsari Barat 2 no. 23',
-        'Golongan Darah' : 'A',
-        'Agama' : 'Islam',
-        'Status Perkawinan' : 'Belum Kawin'
-        },
-    'Aldo':{
-        'ID' : 'DS081097',
-        'Tempat Lahir' : 'Surabaya',
-        'Tanggal Lahir' : '8 Oktober 1997',
-        'Alamat' : 'Kebonagung Blok 2A no. 14',
-        'Golongan Darah' : 'B',
-        'Agama' : 'Islam',
-        'Status Perkawinan' : 'Belum Kawin'
-        }
-}
-
-database_menu_display()
+            print('Mohon pilih ulang (1 / 2):', end = ' ')
+            user_input=input()
